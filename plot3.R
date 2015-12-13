e9f7c95f841f06c4300fd48b08708dfc8c6b38e8
heatherdbcooper <heather.d.b.cooper@gmail.com>
2015-12-13 22:15:18 +0000

    Tidied up the code

--- plot3.R
+++ plot3.R
@@ -1,13 +1,11 @@
 # Set the working directory
 setwd("C:/Users/Heather/Documents/GitHub/ExData_Plotting1")
 
-# Set the column names for the data
-cols <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-
 # Read in data
 data <- read.table(file="household_power_consumption.txt", sep=";", skip=1, header=FALSE, na.strings=c("?"))
 
-# Set column names
+# Set the column names for the data
+cols <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 names(data) <- cols
 
 # Get subset
