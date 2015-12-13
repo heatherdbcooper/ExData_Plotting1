262ad8bac6f0a629f804f17ea136aff266ff8096
heatherdbcooper <heather.d.b.cooper@gmail.com>
2015-12-13 20:33:40 +0000

    Plot 3 done

--- plot3.R
+++ plot3.R
@@ -18,10 +18,10 @@ data <- subset(data, Date == "2/2/2007" | Date == "1/2/2007")
 data$timestamp <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 
 # Plot
-#png("plot3.png", width = 480, height = 480)
+png("plot3.png", width = 480, height = 480)
 with(data, plot(timestamp, Sub_metering_1, main='', xlab='', ylab='Energy sub metering', type="n"))
 with(data, lines(timestamp, Sub_metering_1, col="black"))
 with(data, lines(timestamp, Sub_metering_2, col="red"))
 with(data, lines(timestamp, Sub_metering_3, col="blue"))
 legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-#dev.off()
+dev.off()
